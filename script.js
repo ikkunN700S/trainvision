f111149a0323e44dcfda4b61a5d4a735d9dbf2e1
ikkunN700S <135494855+ikkunN700S@users.noreply.github.com>
2026-09-03 17:54:00 +0900

    v0.1

---
+++ script.js
@@ -0,0 +1,6 @@
+// 現段階ではレイアウト作成がメインのため、JavaScriptは待機状態です。
+// 今後、このファイルに駅名の切り替えや、下半分の路線図アニメーションなどを追加していきます。
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Train Vision Simulator: Step 1 Loaded.");
+});
